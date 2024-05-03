2f21e7125c213b7e50b4991e4133d646811250ab
stijn-castelyns <stijn@u2u.be>
2024-05-03 09:06:03 +0200

    Fixed minimization of chat component

--- BlazorChat/tailwind.config.js
+++ BlazorChat/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./**/*.{razor,html,cshtml}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                bounce: 'bounce 0.6s infinite'
+            },
+            keyframes: {
+                bounce: {
+                    '0%, 100%': { transform: 'translateY(-25%)', opacity: '0.3' },
+                    '50%': { transform: 'translateY(0)', opacity: '1' },
+                }
+            }
+        }
     },
     plugins: [
         require("@tailwindcss/typography")
